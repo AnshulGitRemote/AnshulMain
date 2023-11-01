1405bdc1f4b05bfbc1938586757cde8799440124
AnshulGitRemote <anshulchaware2694@gmail.com>
2023-11-01 15:25:21 +0530

    [Anshul] Adding My new Project

---
+++ src/test/java/demo/Myntra.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Myntra {
+  @Test
+  public void MyntraTest() throws InterruptedException {
+	  WebDriver driver= new ChromeDriver();
+	  driver.get("https://www.myntra.com/");
+	  driver.close();
+	  Thread.sleep(4000);
+  }
+}
